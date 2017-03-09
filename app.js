352fd276d2fb98f0331a3829649276b5498b3565
sunminxiang <minxiang.sun@zhaopin.com.cn>
2017-03-09 13:24:51 +0800

    删除

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var defaults = require('./router/default');
-var config = require('./config/env/development');
-var app = express();
-
-app.use('/',defaults)
-
--- config/config.js
+++
@@ -1,4 +0,0 @@
-//var config = null;
-//if(process && process.env && process.env.NODE_ENV){
-//	config = require()
-//}
